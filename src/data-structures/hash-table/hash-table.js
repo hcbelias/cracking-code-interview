d1ea01d6ca1a9d34275ce5f8f3323f5523c70f76
Henrique Elias <henrique.elias@hotmart.com>
2021-09-07 22:40:01 -0300

    start to work

--- src/data-structures/hash-table/hash-table.js
+++ src/data-structures/hash-table/hash-table.js
@@ -1,5 +1,27 @@
-export default function () {
-	const teste = 1 + 2;
-	const teste2 = teste + 1;
-	return teste + teste2;
+export default class HashTable {
+  constructor() {
+    this.table = new Array(128);
+    this.size = 0;
+  }
+
+  get(key) {
+    const index = this._hash(key);
+
+    return this.table[index];
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+
+    this.table[index] = value;
+  }
+
+  // https://www.freecodecamp.org/news/javascript-hash-table-associative-array-hashing-in-js/
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
 }
--- src/data-structures/hash-table/hash-table.test.js
+++ src/data-structures/hash-table/hash-table.test.js
@@ -1,5 +1,22 @@
 import HashTable from './hash-table';
-debugger;
-test('adds 1 + 2 to equal 3', () => {
-	expect(HashTable(1, 2)).toBe(7);
+
+describe('data-structures/hash-table', function () {
+  describe('set method', function () {
+    it('adds 1 + 2 to equal 3', () => {
+      debugger;
+      expect(HashTable(1, 2)).toBe(7);
+    });
+  });
+  describe('get method', function () {
+    it('adds 1 + 2 to equal 3', () => {
+      debugger;
+      expect(HashTable(1, 2)).toBe(7);
+    });
+  });
+  describe('_hash method', function () {
+    it('adds 1 + 2 to equal 3', () => {
+      debugger;
+      expect(HashTable(1, 2)).toBe(7);
+    });
+  });
 });
